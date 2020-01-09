71d66b26a67a2eaffe7387070ddc795ae7f323c1
Gerson <gerson.toribio@pucp.pe>
2020-01-09 17:35:34 -0500

    Hotfix Functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './components/home';
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Home />
